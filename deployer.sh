172f405a78d913be62efb767fe93f47abfb10d4b
EC2 Default User <ec2-user@ip-10-0-1-105.ec2.internal>
2018-10-29 00:29:06 +0000

    first commit

---
+++ deployer.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo -n "Enter the S3 Bucket to hold lambda package > "
+read S3Bucket
+FILE="$(uuidgen)"
+pip install -r requirements.txt -t "$PWD"
+aws cloudformation package --template-file lambda-cloudformation.yaml --s3-bucket $S3Bucket --s3-prefix custom-lookup/codebuild --output-template-file $FILE
+aws cloudformation deploy --template-file $FILE --stack-name team01-custom-lookup-lambda --capabilities CAPABILITY_NAMED_IAM
+rm $FILE
